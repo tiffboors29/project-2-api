e517076d8ab737d69f46bc44346ff84a27d7f3d0
tiffboors29 <tiffanyboorsma@gmail.com>
2015-08-06 09:35:18 -0400

    Reconfigured Rack Cors following gem install readme

--- config/application.rb
+++ config/application.rb
@@ -34,11 +34,12 @@ module NgApi
 
 
 
-    config.middleware.use Rack::Cors do
-     allow do
-       origins '*'
-       resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options]
-     end
-   end
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
   end
 end
