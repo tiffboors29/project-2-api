ecc3f18012dd5f733a35bed0dda5040e76c92caa
tiffboors29 <tiffanyboorsma@gmail.com>
2015-08-05 20:26:34 -0400

    Add route for post/create /jobs

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   post '/users' => 'users#create'
 
   resources :jobs
+
+  post '/jobs' => 'jobs#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
