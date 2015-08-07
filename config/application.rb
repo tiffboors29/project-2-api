7f1dec3ad572166cc02d7d8df7bfec9ccb86c4a1
tiffboors29 <tiffanyboorsma@gmail.com>
2015-08-07 09:17:45 -0400

    Fix rack cors routes for :patch, :delete

--- config/application.rb
+++ config/application.rb
@@ -34,10 +34,10 @@ module NgApi
 
 
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.use Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options]
       end
     end
 
