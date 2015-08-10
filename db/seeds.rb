ffacaeeacd6bc8a31fe4dbab11de2c016a4a6efd
tiffboors29 <tiffanyboorsma@gmail.com>
2015-08-10 10:03:41 -0400

    Delete all seed data add only users first

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,17 +13,17 @@ User.create!([
     {given_name: "Lester", surname: "Averman", email: "lester@ducks.com", password: "abc123"}
 ])
 
-Job.create!([
-  {title: "Fender", description: "Re-Frett & Full Setup", status: "In Progress", user_id: 1, cost: 75.25},
-  {title: "Gibson", description: "Re-String", status: "Ready for Pickup", user_id: 1, cost: 75.25},
-  {title: "Taylor", description: "Full Setup", status: "In Progress", user_id: 2, cost: 60.25},
-  {title: "Epiphone", description: "Raise Action", status: "In Progress", user_id: 3, cost: 25.25},
-  {title: "Acoustic", description: "Re-String & Frett Polish", status: "Ready for Pickup", user_id: 4, cost: 45.25},
-  {title: "Jazz Bass", description: "Re-Frett & Full Setup", status: "In Progress", user_id: 5, cost: 75.25},
-  {title: "Super Strat", description: "Sparkle Flake Finnish", status: "Ready for Pickup", user_id: 6, cost: 105.25},
-  {title: "Fender", description: "Full Setup", status: "In Progress", user_id: 3, cost: 60.25},
-  {title: "Epiphone", description: "Raise Action", status: "In Progress", user_id: 4, cost: 25.25}
-])
+# Job.create!([
+#   {title: "Fender", description: "Re-Frett & Full Setup", status: "In Progress", user_id: 1, cost: 75.25},
+#   {title: "Gibson", description: "Re-String", status: "Ready for Pickup", user_id: 1, cost: 75.25},
+#   {title: "Taylor", description: "Full Setup", status: "In Progress", user_id: 2, cost: 60.25},
+#   {title: "Epiphone", description: "Raise Action", status: "In Progress", user_id: 3, cost: 25.25},
+#   {title: "Acoustic", description: "Re-String & Frett Polish", status: "Ready for Pickup", user_id: 4, cost: 45.25},
+#   {title: "Jazz Bass", description: "Re-Frett & Full Setup", status: "In Progress", user_id: 5, cost: 75.25},
+#   {title: "Super Strat", description: "Sparkle Flake Finnish", status: "Ready for Pickup", user_id: 6, cost: 105.25},
+#   {title: "Fender", description: "Full Setup", status: "In Progress", user_id: 3, cost: 60.25},
+#   {title: "Epiphone", description: "Raise Action", status: "In Progress", user_id: 4, cost: 25.25}
+# ])
 
 
 # This file should contain all the record creation needed to seed the database with its default values.
